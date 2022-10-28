50093d375a40a0aec93c1095ec9ba3b44aee8cb3
griger <gustavo.rg92@gmail.com>
2022-10-28 09:40:10 +0200

    Añadiendo funcionalidad a la calculadora

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 
+using namespace std;
+
+int main(){
+	int a, b;
+
+	cout << "Dame dos numeros: " << endl;
+	cin >> a >> b;
+
+	cout << "La suma de los dos numeros es: " << a + b << endl;
+
+	return 0;
+}
+
